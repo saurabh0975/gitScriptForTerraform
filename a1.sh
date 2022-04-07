45c3a0fa03ffa89c509ffe49e337094970c663cc
saurabh0975 <93318336+saurabh0975@users.noreply.github.com>
2022-04-07 09:51:36 +0530

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -1,13 +1,12 @@
 sudo apt-get update
 sudo apt-get install -y unzip
 cd /home/ubuntu
-rm -rf terraform_0.14.5_linux_amd64.zip
-wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
-unzip terraform_0.14.5_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/1.1.7/terraform_1.1.7_linux_amd64.zip
+unzip terraform_1.1.7_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 echo "sn1-> sn10"
 echo "Installed Terraform"
-rm -rf terraform_0.14.5_linux_amd64.zip
+rm -rf terraform_1.1.7_linux_amd64.zip
 
 mkdir deployAWS
 cd deployAWS
